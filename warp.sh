5c70b1b26112303c0f9a6b78c936629b20cdbe53
westsidedev <herdersonriker@gmail.com>
2025-01-26 23:36:52 -0300

    update warp script

--- warp.sh
+++ warp.sh
@@ -1,12 +1,53 @@
-#!/bin/sh
-YELLOW="\033[01;93m"
-WHITE="\033[01;97m"
-CLOSE="\033[m"
-nohup warp-svc &
-sleep 2
-warp-cli registration new
-warp-cli connect
-sleep 6
-IP=$(curl https://ifconfig.me)
-clear
-echo -e $YELLOW"IP:$WHITE$IP$CLOSE"
+#!/bin/bash
+warp_proxy(){
+  nohup warp-svc &
+  sleep 2
+  warp-cli registration new
+  warp-cli mode proxy
+  warp-cli proxy port $2
+  warp-cli connect
+  tail -f /dev/null
+}
+
+warp_connect(){
+  nohup warp-svc &
+  sleep 2
+  warp-cli registration new
+  warp-cli connect
+}
+
+docker_start(){
+  for (( i=1; i<=$2; i++ ));do
+    PORT=$((40000 + i));
+    NAME="proxy$i";
+    docker rm proxy$i &> /dev/null
+    docker run -d -e PROXY_PORT=$PORT --network host --name $NAME warp-proxy &> /dev/null
+    echo -e "PROXY => socks5://localhost:$PORT"
+    sleep 1
+  done
+}
+
+test_ip(){
+  for (( i=1; i<=$2; i++ ));do
+    PORT=$((40000 + i))
+    PROXY="socks5://localhost:$PORT"
+    IP=$(curl --proxy $PROXY https://ifconfig.me)
+    echo -e "PORT-IP\n$PORT - $IP"
+  done  
+}
+
+if [[ $1 == "--port" ]];then
+  warp_proxy $@
+elif [[ $1 == "--docker" ]];then
+  docker_start $@
+elif [[ $1 == "--test" ]];then
+  test_ip $@
+elif [[ $1 == "--connect" ]];then
+  warp_connect
+elif [[ $1 == "--open" ]];then
+  docker run --privileged -it -e OPTION=NONE --rm warp-proxy
+elif [[ $1 == "NONE" ]];then
+  bash
+else
+  echo -e "\033[01;91mINVALID ARGUMENT\033[m"
+fi
